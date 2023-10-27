a1f571b8d823a1dc1b24dfe47f2e4a85fd7db903
abishek-as <abishek.codes@outlook.com>
2023-10-27 18:16:19 +0530

    add: Navbar and kinde auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Fira_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import Navbar from "@/components/Navbar";
 
 const font = Fira_Sans({ weight: "400", subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
                     font.className
                 )}
             >
+                <Navbar />
                 {children}
             </body>
         </html>
